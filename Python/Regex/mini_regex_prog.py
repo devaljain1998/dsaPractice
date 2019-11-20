ef68ddd7698819f15f76ee8fab6df02394d5298a
devaljain1998 <devaljain1998@gmail.com>
2019-11-20 23:15:19 +0530

    Modifications

--- Python/Regex/mini_regex_prog.py
+++ Python/Regex/mini_regex_prog.py
@@ -3,12 +3,38 @@ import re
 #To be completed --> More research is needed
 def vowel_and_consonant_count(string):
     vowel_pattern = '[aeiouAEIOU]'
-    consonant_pattern = 'a-zA-Z'
+    #consonant_pattern = f'[a-zA-Z^{vowel_pattern}]'
 
 def keyword_and_identifier_counter(string):
     #All the keywords of the python (obtained through {import keyword})
     keyword_pattern = 'and|as|assert|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|not|or|pass|print|raise|return|try|while|with|yield'
     identifier_pattern = '[_a-zA-Z]+([0-9][_a-zA-Z])*'
+
+    keywords, identifiers = set(), set()
+    try:
+        test_file = open('test_file.py', "r")
+        file_data = test_file.read().split()
+
+        keywords, identifiers = set(), set()
+
+        keyword_regex = re.compile(keyword_pattern)
+        identifier_regex = re.compile(identifier_pattern)
+
+        for word in file_data:
+            if keyword_regex.match(word):
+                keywords.add(word)
+            elif identifier_regex.match(word):
+                identifiers.add(word)
+
+    # except Exception as e:
+    #     print(f'Error occured: ', e)
+
+    # finally:
+    #     test_file.close()
+
+    #return {'keywords': keywords, 'identifiers': identifiers}
+    return [keywords, identifiers]
+
 
 def words_starts_and_with(character, string):
     pattern = f'{character}|^{character}.*{character}$'
@@ -52,3 +78,9 @@ def is_valid_email(email):
 
 def is_valid_url(url):
     url_pattern = 'http(s)?:\/\/(www\.)?[a-zA-Z][a-zA-Z0-9]*\.[co|in|co\.in|com][\/[a-zA-Z0-9\?=&,]*]'
+
+def main():
+    print(keyword_and_identifier_counter())
+
+if __name__ == "__main__":
+    main()
