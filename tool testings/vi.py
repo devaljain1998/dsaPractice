c73edfeb240ca89832562b00772ca328c505d4ae
devaljain1998 <devaljain1998@gmail.com>
2021-03-10 12:09:32 +0530

    learnt vi and pdb

---
+++ tool testings/vi.py
@@ -0,0 +1,10 @@
+# @Created: Wednesday, 10th March, 2021
+# This is file is to test the VI code
+# Author: Deval Sethi
+
+def main(*args, **kwargs):
+	import pdb; pdb.set_trace()
+	print('Hello World')
+
+if __name__ == "__main__":
+	main()
